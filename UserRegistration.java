59f375433743e03f34fa6b5c8b5c899c4e63eb9f
Omkeshwari Pilare <omkeshwari1997@gmail.com>
2020-09-29 20:08:13 +0530

    UC1 Commit

--- UserRegistration.java
+++ UserRegistration.java
@@ -1,9 +1,29 @@
 import java.util.*;
+import java.util.regex.*;
 
 public class UserRegistration
 {
+    public void validFirstName(String fname)
+    {
+        String regex="^[A-Z]{1}[a-zA-z]{2,}";
+        Pattern pattern=Pattern.compile(regex);
+        Matcher matcher=pattern.matcher(fname);
+
+        if(matcher.find())
+            System.out.println(fname+" is valid");
+        else
+            System.out.println(fname+" is not valid");
+    }
+
     public static void main(String[] args)
     {
-        System.out.println("User Registration Pattern Validate");
+        UserRegistration urobj=new UserRegistration();
+        
+        //First name
+        urobj.validFirstName("Abc");
+        urobj.validFirstName("abc");
+        urobj.validFirstName("Ab");
+        urobj.validFirstName("Abcd");
+        urobj.validFirstName("Ab1");
     }
 }
