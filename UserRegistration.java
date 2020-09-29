e81254063a9dd616433f5fd9c4da4d6b53164660
Omkeshwari Pilare <omkeshwari1997@gmail.com>
2020-09-29 23:42:05 +0530

    UC3 Commit

--- UserRegistration.java
+++ UserRegistration.java
@@ -15,6 +15,19 @@ public class UserRegistration
             System.out.println(name+" is not valid");
     }
 
+    public void validemail(String email)
+    {
+        String regex="^[a-z0-9_-]+[.|+]?[a-z0-9_-]+[@][a-z0-9]{1,}[.][a-z]{2,}[.]{0,1}[a-z]*$";
+        //String regex="^[+a-z0-9_-]+[.]{0,1}[a-z0-9_-]+[@][a-z0-9]{1,}[.][a-z]{2,}[.]{0,1}[a-z]*";
+        Pattern pattern=Pattern.compile(regex);
+        Matcher matcher=pattern.matcher(email);
+
+        if(matcher.find())
+            System.out.println(email+" is valid");
+        else
+            System.out.println(email+" is not valid");
+    }
+
     public static void main(String[] args)
     {
         UserRegistration urobj=new UserRegistration();
@@ -27,10 +40,38 @@ public class UserRegistration
         urobj.validName("Ab1");*/
 
         //Last name
-        urobj.validName("ABCD");
+        /*urobj.validName("ABCD");
         urobj.validName("123");
         urobj.validName("A1d");
         urobj.validName("Ab1c3");
-        urobj.validName("Abd1");
+        urobj.validName("Abd1");*/
+
+        //Email
+        urobj.validemail("abc.xyz@bl.co.in");
+        urobj.validemail("abc@bl.co");
+        urobj.validemail("abc@yahoo.com");
+        urobj.validemail("abc-100@yahoo.com");
+        urobj.validemail("abc.100@yahoo.com");
+        urobj.validemail("abc111@abc.com");
+        urobj.validemail("abc-100@abc.net");
+        urobj.validemail("abc.100@abc.com.au");
+        urobj.validemail("abc@1.com");
+        urobj.validemail("abc@gmail.com.com");
+        urobj.validemail("abc+100@gmail.com");
+        //Inavlid email
+        urobj.validemail("abc");
+        urobj.validemail("abc@.com.my");
+        urobj.validemail("abc123@gmail.a");
+        urobj.validemail("abc123@.com");
+        urobj.validemail("abc123@.com.com");
+        urobj.validemail(".abc@abc.com");
+        urobj.validemail("abc()*@gmail.com");
+        urobj.validemail("abc@%*.com");
+        urobj.validemail("abc..2002@gmail.com");
+        urobj.validemail("abc.@gmail.com");
+        urobj.validemail("abc@abc@gmail.com");
+        urobj.validemail("abc@gmail.com.1a");
+        urobj.validemail("abc@gmail.com.aa.au");
+
     }
 }
